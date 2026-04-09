992534d750c914140d1abc59936ba957eae654bb
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-04-10 04:09:03 +0600

    chore: migrate to vite 8

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,6 @@ export default defineConfig({
     plugins: [tailwindcss()],
     build: {
         outDir: "dist",
-        minify: "terser",
-        terserOptions: {
-            ecma: 6,
-            compress: {
-                arguments: true,
-                booleans_as_integers: true,
-                drop_console: true,
-                hoist_funs: true,
-                keep_fargs: false,
-                passes: 3,
-            },
-            format: {
-                comments: false,
-            },
-        },
     },
     server: {},
 });
