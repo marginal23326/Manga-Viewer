46df21aa038dc0d4ee7c6b7f09b00f2b9b31f3c8
marginal23326 <58261815+marginal23326@users.noreply.github.com>
2026-08-01 23:45:22 +0600

    refactor: use exponentiation

--- src/js/core/Utils.js
+++ src/js/core/Utils.js
@@ -37,7 +37,7 @@ export function getChapterBounds(manga, chapterIndex) {
 }
 
 export function easeInOutCubic(t) {
-    return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+    return t < 0.5 ? 4 * t * t * t : 1 - (-2 * t + 2) ** 3 / 2;
 }
 
 export function scrollToView(element, behavior = "smooth", block = "start") {
